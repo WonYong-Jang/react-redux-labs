754c863a83df321221a5632b9962664cb18a4a15
wonyong <zcx5674@gmail.com>
2018-06-08 17:40:46 +0900

    add search function with sort and filter funtion

--- react-fundamentals/src/components/App.js
+++ react-fundamentals/src/components/App.js
@@ -1,18 +1,11 @@
 import React from 'react'; // ES6 문법
+import Contact from './Contact';
  
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: ''
-        };
-    }
+    
     render(){
         return (
-           <div>
-            <button onClick={() => {this.setState({name: 'wonyong!'}); }}>Click me</button>
-            <h1>Hello !!! {this.state.name} </h1>
-           </div> 
+          <Contact/>
         );
     }
 }
